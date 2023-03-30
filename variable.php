1fdf2563a9be16cadd813266a502a9f39f7b687c
naadflwrx <itssinaa1904@gmail.com>
2023-03-30 12:13:30 +0700

    praktikum dan tugas

---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Data Diri Mahasiswa";
+// jika nama variabelnya sama, tetapi memiliki nilai yang berbeda
+// maka yang di cetak adalah variable yang kedua
+
+echo $nama;
+// ==============================================
+// definisikan variable data diri
+$nama_saya = "St. Nurmuhsina";
+$umur = 18;
+$berat = 45;
+
+echo '<br/>Nama : ' .$nama_saya;
+echo "<br/>Umur saya adalah $umur Tahun";
+echo "<br/>Berat saya $berat KG";
+?>
